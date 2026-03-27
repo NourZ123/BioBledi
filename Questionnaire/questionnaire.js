2c599806c678a1747260671865d9955f8f5547ac
nour <nour.zahrouni@ensi-uma.tn>
2026-03-27 01:45:25 +0100

    questionnaire

--- Questionnaire/questionnaire.js
+++ Questionnaire/questionnaire.js
@@ -2,11 +2,27 @@ let formulaire = document.getElementById("formulaire-avis");
 formulaire.addEventListener("submit", function (event) {
   let nom = document.getElementById("nom");
   let email = document.getElementById("mail");
+  let prixRadio = document.getElementsByName("prix");
+  let notesRadio = document.getElementsByName("note"); // Appréciation globale
   let champsdate = document.getElementById("date");
   let dateactuelle = new Date();
   let dateSaisie = new Date(champsdate.value);
-  let dateMinimum = new Date("2026-02-01");
+  let dateMinimum = new Date("2025-01-01");
+  let message = document.getElementById("msg");
   let alpha = /^[a-zA-Z ]+$/;
+  if (
+    nom.value === "" ||
+    email.value === "" ||
+    champsdate.value === "" ||
+    message.value === ""
+  ) {
+    alert(
+      "Veuillez remplir tous les champs obligatoires (Nom, Email, Date et Suggestions)."
+    );
+    event.preventDefault();
+    return;
+  }
+
   if (!alpha.test(nom.value)) {
     alert("Veuillez saisir un nom valide");
     event.preventDefault();
@@ -17,7 +33,7 @@ formulaire.addEventListener("submit", function (event) {
   }
   if (dateSaisie < dateMinimum || dateSaisie > dateactuelle) {
     alert(
-      "Veuillez saisir une date valide (à partir de 2026 et jusqu'à aujourd'hui)."
+      "Veuillez saisir une date valide (à partir de 2025 et jusqu'à aujourd'hui)."
     );
     event.preventDefault();
   }
