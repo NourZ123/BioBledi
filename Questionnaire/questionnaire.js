7f51733f6c4a228b8141bfd9b7fb76224efd78a1
nour <nour.zahrouni@ensi-uma.tn>
2026-03-29 12:11:35 +0100

    quest

--- Questionnaire/questionnaire.js
+++ Questionnaire/questionnaire.js
@@ -2,14 +2,13 @@ let formulaire = document.getElementById("formulaire-avis");
 formulaire.addEventListener("submit", function (event) {
   let nom = document.getElementById("nom");
   let email = document.getElementById("mail");
-  let prixRadio = document.getElementsByName("prix");
-  let notesRadio = document.getElementsByName("note"); // Appréciation globale
   let champsdate = document.getElementById("date");
   let dateactuelle = new Date();
   let dateSaisie = new Date(champsdate.value);
   let dateMinimum = new Date("2025-01-01");
   let message = document.getElementById("msg");
   let alpha = /^[a-zA-Z ]+$/;
+  //Tous les champs doivent être remplies
   if (
     nom.value === "" ||
     email.value === "" ||
@@ -22,15 +21,17 @@ formulaire.addEventListener("submit", function (event) {
     event.preventDefault();
     return;
   }
-
+  // Le nom du client doit etre valide: ne contient que des caractères alphabétiques
   if (!alpha.test(nom.value)) {
     alert("Veuillez saisir un nom valide");
     event.preventDefault();
   }
+  // l'adressse mail doit être valide
   if (!email.value.includes("@")) {
     alert("Veuillez saisir une adresse mail valide");
     event.preventDefault();
   }
+  // la date du dernier achat doit être inferieure ou égale à la date actuelle et supérieure à la date de création du projet
   if (dateSaisie < dateMinimum || dateSaisie > dateactuelle) {
     alert(
       "Veuillez saisir une date valide (à partir de 2025 et jusqu'à aujourd'hui)."
